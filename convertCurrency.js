540a46028a4472578fb0cf689a7045c36fcf50c4
ricardoazev <84046316+ricardoazev@users.noreply.github.com>
2024-01-19 12:17:57 -0300

    Add files via upload

---
+++ convertCurrency.js
@@ -0,0 +1,71 @@
+import { LightningElement } from 'lwc';
+
+export default class ConvertCurrency extends LightningElement {
+    showOutput = false;
+    convertedValue = "";
+    toCurrency = "";
+    fromCurrency = "";
+    enteredamount = "";
+    currencyOptions = [];
+
+    connectedCallback() {
+        this.fetchSymbols();
+    }
+
+    changeHandler(event) {
+      let {name, value} = event.target;
+      if(name === 'amount') this.enteredamount = value;
+      if(name === 'fromcurr') this.fromCurrency = value;
+      if(name === 'tocurr') this.toCurrency = value;
+    }
+
+    clickHandler() {
+      this.conversion()
+    }
+
+    async fetchSymbols() {
+       let endpoint = 'https://api.frankfurter.app/currencies';
+       try {
+        let response = await fetch(endpoint);
+        if(!response.ok){
+            throw new Error('A rede não estava ok')
+        }
+        const data = await response.json()
+        //process the data returned from API
+        let options = [];
+        for(let symbol in data) {
+            options = [...options,{label:symbol,value:symbol}]
+        }
+
+        this.currencyOptions = [...options]
+       } catch (error) {
+        console.log(error)
+       }
+    }
+
+    async conversion() {
+        /**
+         * const host = 'api.frankfurter.app';
+fetch(`https://${host}/latest?amount=10&from=GBP&to=USD`)
+  .then(resp => resp.json())
+  .then((data) => {
+    alert(`10 GBP = ${data.rates.USD} USD`);
+  });
+         */
+        let endpoint = `https://api.frankfurter.app/latest?amount=${this.enteredamount}&from=${this.fromCurrency}&to=${this.toCurrency}`;
+        try {
+            let response = await fetch(endpoint);
+            if(!response.ok){
+                throw new Error('A rede não estava ok')
+            }
+            const data = await response.json()
+            //process the data returned from API
+            
+    
+            this.convertedValue = data.rates[this.toCurrency]
+            this.showOutput = true;
+           } catch (error) {
+            console.log(error)
+           }
+    }
+}
